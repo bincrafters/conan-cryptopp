e3f4829c1d182968b69634bfedb60c429a127a75
dimitri <dimi309@users.noreply.github.com>
2017-11-30 07:50:32 +0100

    Removed VS build.

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, ConfigureEnvironment, VisualStudioBuildEnvironment
+from conans import ConanFile, CMake
 from conans import tools
 import os
 
@@ -7,61 +7,45 @@ class CryptoPPConan(ConanFile):
     version = "5.6.5"
     url = "https://github.com/bincrafters/conan-cryptopp"
     description = "Crypto++ Library is a free C++ class library of cryptographic schemes."
+    sources_folder = "sources"
+    generators = ["cmake", "txt"]
     settings = "os", "compiler", "build_type", "arch"
     license = "Boost Software License 1.0"
-    options = {"shared": [True, False]}
-    default_options = "shared=True"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = "shared=False", "fPIC=True"
+    exports_sources = "CMakeLists.txt"
+
+    def configure(self):
+        del self.settings.compiler.libcxx
+
+        if self.settings.os == "Windows":
+            self.options.remove("fPIC")
 
     def source(self):
-        zipname = 'cryptopp565.zip'
-        url = 'http://cryptopp.com/%s' % zipname
-        sha256 = 'a75ef486fe3128008bbb201efee3dcdcffbe791120952910883b26337ec32c34'
+        zipname = 'CRYPTOPP_5_6_5.tar.gz'
+        url = 'https://github.com/weidai11/cryptopp/archive/%s' % zipname
         tools.download(url, zipname)
-        tools.check_sha256(zipname, sha256)
         tools.unzip(zipname)
         os.unlink(zipname)
-
-        bad_lines = (
-            "msbuild /t:Build /p:Configuration=Debug;Platform=Win32 cryptlib.vcxproj",
-            "msbuild /t:Build /p:Configuration=Debug;Platform=Win32 cryptest.vcxproj",
-            "Win32\\output\\debug\\cryptest.exe mac_dll \"$(TargetPath)\"",
-            "IF %ERRORLEVEL% EQU 0 (echo mac done &gt; \"$(OutDir)\"\\cryptopp.mac.done)")
-        for line in bad_lines:
-            tools.replace_in_file("cryptdll.vcxproj", search=line, replace="", strict=True)
-
-    def makefile_build(self):
-        env = ConfigureEnvironment(self.deps_cpp_info, self.settings)
-        if self.options.shared:
-            self.run('%s make dynamic' % env.command_line)
-        else:
-            self.run('%s make static' % env.command_line)
-        if self.scope.build_tests:
-            self.run('%s make test check' % env.command_line)
-
-    def msvc_build(self):
-        env = VisualStudioBuildEnvironment(self)
-        with tools.environment_append(env.vars):
-            vcvars = tools.vcvars_command(self.settings)
-
-            build_command = tools.build_sln_command(self.settings, "cryptest.sln")
-            build_command = build_command.replace("x86", "Win32")
-
-            if self.options.shared:
-                build_command = build_command.replace('%s' % self.settings.build_type,
-                                                          '"DLL-Import %s"' % self.settings.build_type)
-            self.output.info("Build command: %s" % build_command)
-            self.run("%s && %s" % (vcvars, build_command))
+        os.rename("cryptopp-CRYPTOPP_5_6_5", self.sources_folder)
 
     def build(self):
-        if str(self.settings.os) != "Windows":
-            return self.makefile_build()
-        else:
-            return self.msvc_build()
+        cmake = CMake(self)
+        if self.settings.os != "Windows":
+            cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.fPIC
+        cmake.definitions["BUILD_STATIC"] = not self.options.shared
+        cmake.definitions["BUILD_SHARED"] = self.options.shared
+        cmake.definitions["BUILD_TESTING"] = False
+        cmake.definitions["BUILD_DOCUMENTATION"] = False
+        cmake.configure()
+        cmake.build()
 
     def package(self):
-        self.copy(pattern="*.h", dst="include/cryptopp", src=".")
+        self.copy(pattern="*.h", dst="include/cryptopp", src=".", keep_path=False)
         self.copy(pattern="*.so", dst="lib", src=".", keep_path=False)
         self.copy(pattern="*.dll", dst="bin", src=".", keep_path=False)
+        if self.settings.build_type == "Debug":
+            self.copy(pattern="*.pdb", dst="bin", src=".", keep_path=False)
         self.copy(pattern="*.dylib", dst="lib", src=".", keep_path=False)
         self.copy(pattern="*.a", dst="lib", src=".", keep_path=False)
         self.copy(pattern="*.lib", dst="lib", src=".", keep_path=False)
@@ -71,6 +55,6 @@ class CryptoPPConan(ConanFile):
             self.cpp_info.libs = ["cryptopp"]
         else:
             if self.options.shared:
-                self.cpp_info.libs = ["cryptopp"]
+                self.cpp_info.libs = ["cryptopp-shared"]
             else:
-                self.cpp_info.libs = ["cryptlib"]
+                self.cpp_info.libs = ["cryptopp-static"]
