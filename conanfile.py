4903159cc3dc7a13089c0cc48ab1ecb69d8963fc
SSE4 <tomskside@gmail.com>
2018-11-25 18:08:22 +0700

    - apply bincrafters conventions
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -2,18 +2,19 @@ from conans import ConanFile, CMake
 from conans import tools
 import os
 
+
 class CryptoPPConan(ConanFile):
     name = "cryptopp"
     version = "5.6.5"
     url = "https://github.com/bincrafters/conan-cryptopp"
     description = "Crypto++ Library is a free C++ class library of cryptographic schemes."
-    sources_folder = "sources"
     generators = "cmake"
     settings = "os", "compiler", "build_type", "arch"
     license = "https://github.com/weidai11/cryptopp/blob/master/License.txt"
     options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = "shared=False", "fPIC=True"
+    default_options = {'shared': False, 'fPIC': True}
     exports_sources = "CMakeLists.txt"
+    _source_subfolder = "sources"
 
     def configure(self):
         if self.settings.os == "Windows":
@@ -21,17 +22,12 @@ class CryptoPPConan(ConanFile):
 
     def source(self):
         zipname = 'CRYPTOPP_5_6_5.tar.gz'
-        url = 'https://github.com/weidai11/cryptopp/archive/%s' % zipname
-        tools.download(url, zipname)
-        tools.unzip(zipname)
-        os.unlink(zipname)
-        os.rename("cryptopp-CRYPTOPP_5_6_5", self.sources_folder)
+        tools.get('https://github.com/weidai11/cryptopp/archive/%s' % zipname)
+        os.rename("cryptopp-CRYPTOPP_5_6_5", self._source_subfolder)
 
     def build(self):
         cmake = CMake(self)
-        if self.settings.os != "Windows":
-            cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.fPIC
-        else:
+        if self.settings.os == "Windows":
             cmake.definitions["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = self.options.shared
         cmake.definitions["BUILD_STATIC"] = not self.options.shared
         cmake.definitions["BUILD_SHARED"] = self.options.shared
@@ -41,7 +37,7 @@ class CryptoPPConan(ConanFile):
         cmake.build()
 
     def package(self):
-        self.copy(pattern="License*", dst="licenses", src=self.sources_folder, ignore_case=True, keep_path=False)
+        self.copy(pattern="License*", dst="licenses", src=self._source_subfolder, ignore_case=True, keep_path=False)
         self.copy(pattern="*.h", dst="include/cryptopp", src=".", keep_path=False)
         self.copy(pattern="*.so", dst="lib", src=".", keep_path=False)
         self.copy(pattern="*.dll", dst="bin", src=".", keep_path=False)
